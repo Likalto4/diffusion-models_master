c5c48a484227c056ea3124b0a732bd66bb1780f2
Likalto4 <ricardo.montoya.da@gmail.com>
2023-03-20 13:08:31 +0100

    save_RGB_pngs clean and automated

--- dataset_analysis/save_pngs/save_RGB_pngs.py
+++ dataset_analysis/save_pngs/save_RGB_pngs.py
@@ -19,40 +19,38 @@ import yaml
 
 def main():
     # load the config file
-    config_path = repo_path / "dataset_analysis/config_latent.yaml"
+    config_path = "png_saving.yaml"
     with open(config_path) as file: # expects the config file to be in the same directory
             config = yaml.load(file, Loader=yaml.FullLoader)
 
-    folder_dir = repo_path / config['processing']['dataset']
+    folder_dir = repo_path / config['processing']['dataset_path']
     # create the save directory if needed
-    saving_dir = folder_dir.parent / "breast10p_RGB"
+    saving_dir = folder_dir.parent / config['processing']['saving_name']
     saving_dir.mkdir(parents=False, exist_ok=True)
     
     preprocess = Compose(
-        [
-            Resize(config['processing']['resolution'], interpolation= InterpolationMode.BILINEAR), #getattr(InterpolationMode, config['processing']['interpolation'])),  # Smaller edge is resized to 256 preserving aspect ratio
-            CenterCrop(config['processing']['resolution']),  # Center crop to the desired squared resolution
+        [ # classic squared aspect-preserved centered image
+            Resize(config['processing']['resolution'], interpolation= InterpolationMode.BILINEAR),
+            CenterCrop(config['processing']['resolution']), 
         ]
     )
-    break_i = None
+    break_i = None # early break for debugging
     for i, filename in enumerate(tqdm(os.listdir(folder_dir), desc="Preprocessing images")):
         if filename.endswith(f".png"):
-            path_ex = folder_dir / filename
+            path_im = folder_dir / filename
             saving_path = saving_dir / filename
             # read the image using PIL
-            im = Image.open(path_ex)
-            # center crop to be square
+            im = Image.open(path_im)
             im_preprocessed = preprocess(im)
-            # convert to numpy array
             im_preprocessed = np.array(im_preprocessed)
             # scale to 0-255 and convert to uint8
             im_uint8 = (im_preprocessed/ 4095.0)*255.0
             im_uint8 = im_uint8.astype(np.uint8)
             # convert to RGB
             im_RGB = np.stack((im_uint8,)*3, axis=-1)
-            # save as png using cv
+            # save as png
             cv.imwrite(str(saving_path), im_RGB)
-            if i == break_i:
+            if i == break_i: # early break for debugging
                 break
             
 if __name__ == "__main__":
