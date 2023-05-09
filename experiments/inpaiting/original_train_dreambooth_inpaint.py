7b1d3e9f892be96c80b21858e40f2f3a1fdd17ea
Likalto4 <ricardo.montoya.da@gmail.com>
2023-05-09 17:42:43 +0200

    medical inpaiting almost ready missing the image logging for the inpaiting

--- experiments/inpaiting/original_train_dreambooth_inpaint.py
+++ experiments/inpaiting/original_train_dreambooth_inpaint.py
@@ -637,7 +637,7 @@ def main():
     # The trackers initializes automatically on the main process.
     if accelerator.is_main_process:
         accelerator.init_trackers("dreambooth", config=vars(args))
-
+    
     # Train!
     total_batch_size = args.train_batch_size * accelerator.num_processes * args.gradient_accumulation_steps
 
